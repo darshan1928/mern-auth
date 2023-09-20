bf24578bae3b88b481a04c1b4af83c08bd2b0383
Darshan Raj <darshankollam8@gmail.com>
2023-09-20 11:09:09 +0530

    added routes and created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 function App() {
-
-  return (
-    <div>
-    </div>
-  )
+    console.log(1);
+    return (
+        <Router>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/signIn" element={<SignIn />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
