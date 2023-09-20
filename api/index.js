0e1c219bf0557f674ea779674812f070169ae7ad
Darshan Raj <darshankollam8@gmail.com>
2023-09-20 12:57:52 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+const PORT = 6000;
+
+const app = express();
+app.use(cors());
+dotenv.config()
 
 
-const app = express()
-const PORT = 6000
+mongoose.connect(process.env.MONGO).then(()=>{console.log("DB connected")}).catch((err)=>{console.log(err);})
 
-
-app.listen(PORT,()=>{console.log(
-    `server running @ ${PORT}`
-)})
-
+app.listen(PORT, () => {
+    console.log(`server running @ ${PORT}`);
+});
